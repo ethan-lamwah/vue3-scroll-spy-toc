9a8afd1cb9526e655e675e26fbfe932843b6d416
Ethan Lam <42923621+ethan-lamwah@users.noreply.github.com>
2022-10-13 23:10:27 -0700

    Update PostContent.vue

--- src/components/PostContent.vue
+++ src/components/PostContent.vue
@@ -24,7 +24,7 @@ const { currentSection, toc } = useScrollToc(
         >Intersection Observer API</a
       >.
     </p>
-    <p>Check out the source code <a href="https://github.com/ethan-lamwah/vue3-scroll-spy-toc">here</a>, enjoys!</p>
+    <p>Check out <a href="https://www.ethanlam.dev/post/dynamic-table-of-contents-with-scroll-spy-effect-i">this post</a> or source code <a href="https://github.com/ethan-lamwah/vue3-scroll-spy-toc">here</a>, enjoys!</p>
   </header>
   <main>
     <section>
